f477b256b076ee20930c268808ed80cdd84e300c
AhmedGhanam <yghnam334455@gmail.com>
2021-12-03 17:53:45 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/material.dart';
+import 'package:library_book/src/models/notifiers/book_notifier.dart';
+import 'package:library_book/src/models/notifiers/theme_notifier.dart';
+import 'package:library_book/src/screens/home_screen.dart';
+import 'package:provider/provider.dart';
+
+
+import 'package:library_book/src/theme/theme.dart' as libraryTheme;
+
+void main() => runApp(BookLibrary());
+
+class BookLibrary extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final themeNotifier = ThemeNotifier();
+    final bookNotifier = BookNotifier();
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => themeNotifier),
+        ChangeNotifierProvider(create: (_) => bookNotifier),
+      ],
+      child: MaterialAppWithTheme(),
+    );
+  }
+}
+
+class MaterialAppWithTheme extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+
+    return MaterialApp(
+      title: 'Book Library',
+      darkTheme: libraryTheme.Theme.darkTheme,
+      theme: themeNotifier.darkModeEnabled
+          ? libraryTheme.Theme.darkTheme
+          : libraryTheme.Theme.lightTheme,
+      home: HomeScreen(),
+    );
+  }
+}
